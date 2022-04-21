582f52a4e88586f03e77e5e21b3baf28eeeae9d1
Godhanded <aezeigwe2003@gmail.com>
2022-04-21 02:45:35 +0100

    Revert "corrected test errors"
    This reverts commit fc7deebdd736814593e84b0deb681e3c1063a031.

--- test/G-DAO.js
+++ test/G-DAO.js
@@ -4,7 +4,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 
-describe ("G-DAO contract", function() {
+describe ("G-DAO contract", fuunction() {
 
     let Gcontract;
     let GDAO;
@@ -17,57 +17,54 @@ describe ("G-DAO contract", function() {
   // time. It receives a callback, which can be async.
      beforeEach (async function(){
        // get contract factory and signers
-       Elect = await ethers.getContractFactory("Elect");
+       Gcontract = await ethers.getContractFactory("G-DAO");
        [owner,addr1, addr2, addr3] = await ethers.getSigners();
        
-       GDAO= await Elect.deploy();
-       await GDAO.setChairman(owner.address);
-      // const chair = await GDAO.Chairman;
+       GDAO= await Gcontract.deploy();
+       const chair = await GDAO.setChairman(owner.address);
        });
 
        it("should set the right owner", async function(){
-          expect(await GDAO.owner()).to.equal(owner.address);
+           expect(await GDAO.owner()).to.equal(owner.address);
        });
 
 
-      // it("should set the right chairman",async function(){
-      //    expect ( chair).to.equal(owner.address);
-      // });
-
+       it("should set the right chairman", async function(){
+           expect(await GDAO.Chairman()).to.equal(chair);
+       });
 
 
        it("should fail if not a stake holder",async function(){
-           await GDAO.beginVote();
-           await expect(GDAO.connect(addr1).voteCandidate(1)).to.be.revertedWith("You are not a stakeholder");
+           await expect(GDAO.voteCandidate(1)).to.be.revertedwith("You are not a stakeholder");
        });
 
 
        it("should fail if its not time to vote",async function(){
            await GDAO.addStakeholder(owner.address);
 
-           await expect (GDAO.voteCandidate(1)).to.be.revertedWith("Its not yet time to vote");
+           await expect (GDAO.voteCandidate(1)).to.be.revertedwith("Its not yet time to vote");
 
        });
 
 
        it("should not set chairman if not owner",async function(){
-           await expect(GDAO.connect(addr1).setChairman(addr2.address)).to.be.revertedWith("Ownable: caller is not the owner");
+           await expect(GDAO.connect(addr1).setChairman(addr2)).to.be.revertedwith("Ownable: caller is not the owner");
        });
 
 
        it("only chairman can enable contract",async function(){
-           await expect(GDAO.connect(addr1).enable()).to.be.revertedWith("You are not the chairman");
+           await expect(GDAO.connect(addr1).enable()).to.be.revertedwith("You are not the chairman");
        });
 
        it ("only chairman can disable contract",async function(){
-           await expect (GDAO.connect(addr1).disable()).to.be.revertedWith("You are not the chairman");
+           await expect (GDAO.connect(addr1).disable()).to.be.revertedwith("You are not the chairman");
        });
 
 
        it("should not vote if not a candidate",async function(){
            await GDAO.connect(owner).beginVote();
            await GDAO.addStakeholder(owner.address);
-          await expect(GDAO.connect(owner).voteCandidate(1)).to.be.revertedWith("This is not a candidate");
+          await expect(GDAO.connect(owner).voteCandidate(1)).to.be.revertedwith("This is not a candidate");
        });
 
 
@@ -81,7 +78,7 @@ describe ("G-DAO contract", function() {
            await GDAO.addcandidate("ed","head","ipfs.io/ipfs/21323244hfjb34j");
             await GDAO.addStakeholder(owner.address);
             await GDAO.beginVote();
-            expect(await GDAO.voteCandidate(1)).to.returns("voted");
+            expect(await GDAO.voteCandidate(1)).to.return("voted");
        });
 });
 
